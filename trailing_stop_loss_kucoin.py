9452006f3af2c125b5c65a25c823bf9001085afd
Olle de Jong <olledejong@gmail.com>
2021-05-23 12:12:07 +0200

    Tiny changes.

--- trailing_stop_loss_kucoin.py
+++ trailing_stop_loss_kucoin.py
@@ -1,8 +1,7 @@
-import json
-import kucoin.client
 from kucoin.client import Client
 import time
 import math
+
 ########################################################################################
 ############################## YOUR STUFF GOES HERE ####################################
 
@@ -13,7 +12,6 @@ api_passphrase = "API_PASSPHRASE"
 ########################################################################################
 
 
-
 client = Client(api_key, api_secret, api_passphrase)
 global currency
 global pair
@@ -22,7 +20,9 @@ global enter
 global holding
 global slprice
 global price_offset
-def findHolding(currency):
+
+
+def find_holding(currency):
     accounts = client.get_accounts()
     found = ""
     index = 0
@@ -48,40 +48,42 @@ def initialize_tracker():
     global slprice
     global price_offset
     print("-------------------------------")
-    holding = math.trunc(float(findHolding(currency)) * 10_000 ) / 10_000
-    poffset = enter * ( offset / 100.0)
-    first_stop_loss =  enter - poffset
+    holding = math.trunc(float(find_holding(currency)) * 10_000) / 10_000
+    poffset = enter * (offset / 100.0)
+    first_stop_loss = enter - poffset
     price_offset = poffset
     slprice = first_stop_loss
     print("First Stop Loss price: " + str(first_stop_loss))
     print("#####################################################################")
     while True:
-        doTick()
+        do_tick()
         time.sleep(10)
-    
 
-def doTick():
+
+def do_tick():
     global price_offset
     global slprice
     this_tick_price = float(client.get_ticker(pair)['price'])
     this_tick_stoploss = this_tick_price - price_offset
     if this_tick_stoploss > slprice:
         print(">>")
-        print("Old Stop Loss: " + str(slprice) )
+        print("Old Stop Loss: " + str(slprice))
         print("New Stop Loss: " + str(this_tick_stoploss))
         slprice = this_tick_stoploss
     if this_tick_price < slprice:
         print("Triggered Stop Loss. Selling out.")
         sellout()
 
+
 def sellout():
     global pair
     global slprice
-    order = client.create_market_order(pair, client.SIDE_SELL, size = holding)
+    order = client.create_market_order(pair, client.SIDE_SELL, size=holding)
     order_details = client.get_order(order["orderId"])
     price = float(order_details["dealFunds"]) / float(order_details["dealSize"])
     print("Sold out @  " + str(price) + "")
     input()
+
 
 def startup():
     global currency
@@ -98,5 +100,6 @@ def startup():
     print("Insert enter price: ")
     enter = float(input())
     initialize_tracker()
-    
+
+
 startup()
